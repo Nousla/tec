d047175d804f70b822eb63bda159b517e273fa3e
Nousla <sirtheorist@gmail.com>
2016-11-14 16:49:57 +0100

    Added command list.

--- WebSocket/websocket_server.js
+++ WebSocket/websocket_server.js
@@ -5,6 +5,9 @@ var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ server: http });
 var url = require('url');
 
+var commands = new Map();
+commands.set('send', send);
+
 express.get('/', function(req, res){
 	res.sendFile(__dirname + '/websocket_client.html');
 });
@@ -12,15 +15,30 @@ express.get('/', function(req, res){
 wss.on('connection', function connection(ws) {
 	url.parse(ws.upgradeReq.url, true);
 
-    ws.on('message', function(event) {
-        wss.clients.forEach(function each(client) {
-			client.send(event);
-		});
-		console.log("Message received: " + event);
+    ws.on('message', function(msg) {
+		if(msg.length == 0){
+			return;
+		}		
+			
+		var msgs = msg.split(" ");
+		var cmd = msgs[0];
+		var args = msgs.slice(1,msgs.length);
+		
+		if(commands.has(cmd)){
+			commands.get(cmd)(ws, args);
+		}
     });
 });
 
 http.on('request', express);
 http.listen(3000, function(){
-  console.log('Listening on ' + port);
+	console.log('Listening on ' + port);
 });
+
+function send(ws, args){
+	msg = args.join(" ");
+	wss.clients.forEach(function each(client) {
+		client.send(msg);
+	});
+	console.log("Message received: " + msg);
+}
