e76ca18a0e97c6c81e28cce1c7488cee5c71fb0e
Nousla <sirtheorist@gmail.com>
2016-11-14 14:40:18 +0100

    WebSocket client now works correctly.

--- WebSocket/websocket_server.js
+++ WebSocket/websocket_server.js
@@ -1,17 +1,16 @@
-timers = {}
-
 var port = 3000;
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express')();
+var http = require('http').createServer();
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ server: http });
+var url = require('url');
 
-app.get('/', function(req, res){
+express.get('/', function(req, res){
 	res.sendFile(__dirname + '/websocket_client.html');
 });
 
 wss.on('connection', function connection(ws) {
-	var location = url.parse(ws.upgradeReq.url, true);
+	url.parse(ws.upgradeReq.url, true);
 
     ws.on('message', function(event) {
         wss.clients.forEach(function each(client) {
@@ -21,6 +20,7 @@ wss.on('connection', function connection(ws) {
     });
 });
 
+http.on('request', express);
 http.listen(3000, function(){
-  console.log('Listening on ' + http.address().port);
+  console.log('Listening on ' + port);
 });
