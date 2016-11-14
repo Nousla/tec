2637d30e4a1ee4ef56098dd5a0633f9bd47b9261
Nousla <sirtheorist@gmail.com>
2016-11-14 18:14:30 +0100

    Username can now be set. Improved command system.

--- WebSocket/websocket_server.js
+++ WebSocket/websocket_server.js
@@ -6,7 +6,8 @@ var wss = new WebSocketServer({ server: http });
 var url = require('url');
 
 var commands = new Map();
-commands.set('send', send);
+commands.set('msg_send', sendMsg);
+commands.set('name_set', setName);
 
 express.get('/', function(req, res){
 	res.sendFile(__dirname + '/websocket_client.html');
@@ -16,16 +17,13 @@ wss.on('connection', function connection(ws) {
 	url.parse(ws.upgradeReq.url, true);
 
     ws.on('message', function(msg) {
-		if(msg.length == 0){
+		var cmd = parseMessage(msg);
+		if(cmd == undefined){
 			return;
-		}		
-			
-		var msgs = msg.split(" ");
-		var cmd = msgs[0];
-		var args = msgs.slice(1,msgs.length);
+		}
 		
-		if(commands.has(cmd)){
-			commands.get(cmd)(ws, args);
+		if(commands.has(cmd.code)){
+			commands.get(cmd.code)(ws, cmd.args);
 		}
     });
 });
@@ -35,10 +33,38 @@ http.listen(3000, function(){
 	console.log('Listening on ' + port);
 });
 
-function send(ws, args){
+function parseMessage(msg){
+	if(msg.length == 0){
+		return;
+	}
+	
+	var msgs = msg.split(" ");
+	var cmd = {
+		code : msgs[0],
+		args : msgs.slice(1,msgs.length)
+	}
+	
+	return cmd;
+}
+
+function sendMsg(ws, args){
 	msg = args.join(" ");
 	wss.clients.forEach(function each(client) {
-		client.send(msg);
+		send(client, 'msg_received', msg);
 	});
 	console.log("Message received: " + msg);
+}
+
+function setName(ws, args){
+	if(args.length != 1){
+		ws.send('name_error');
+	}
+	
+	ws.name = args[0];
+	send(ws, 'name_changed', ws.name);
+	console.log("Changed client name to: " + ws.name);
+}
+
+function send(ws, code, args){
+	ws.send(code + " " + args);
 }
